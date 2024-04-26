34f1af41c10f881e7abc44ef2b56870b715471ee
Tracy Karanja <chirutracy@gmail.com>
2024-04-26 23:10:31 +0300

    first commit

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskList from "./TaskList";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleClick = (category) => {
+    setSelectedCategory(category);
+  };
+  // creating buttons and assigning className of selected to the button that is clicked
+  const categoryButtons = categories.map((category, index) => (
+    <button
+      key={index}
+      onClick={() => handleClick(category)}
+      className={category === selectedCategory ? "selected" : ""}
+    >
+      {category}
+    </button>
+  ));
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-function Task() {
+function Task({ text, category}) {
+  const [tasks, setTasks] = useState([])
+
+  function handleDeleteClick() {
+   const remainingTasks = setTasks(tasks.filter(task => task.text !== text))
+
+   setTasks(remainingTasks)
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={()=> handleDeleteClick()} className="delete">X</button>
+      <div>{}</div>
     </div>
   );
 }
